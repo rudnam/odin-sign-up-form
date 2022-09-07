c38eb9a8ec08a17063872d8c66ff7d3a749b08c8
rudnam <francisalbarracin2@gmail.com>
2022-09-07 18:21:22 +0800

    Implement sign-up form

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Script.js
+const PASSWORD = document.querySelector('#password');
+const CONFIRM = document.querySelector('#confirm-password')
+const ERRORMSG = document.querySelector('#error-msg')
+
+document.addEventListener('keyup',check)
+
+function check(e) {
+    if (PASSWORD.value == CONFIRM.value) {
+        console.log('wow');
+        PASSWORD.classList.remove('error');
+        CONFIRM.classList.remove('error');
+        ERRORMSG.hidden = true;
+    } else {
+        console.log('no');
+        PASSWORD.classList.add('error');
+        CONFIRM.classList.add('error');
+        ERRORMSG.hidden = false;
+    }
+}
