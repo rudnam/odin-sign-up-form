23e4a6e17376e7e2a3cde45c07ee226c83c52492
rudnam <francisalbarracin2@gmail.com>
2022-09-07 18:39:24 +0800

    Remove console logs

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ document.addEventListener('keyup',check)
 
 function check(e) {
     if (PASSWORD.value == CONFIRM.value) {
-        console.log('wow');
         PASSWORD.classList.remove('error');
         CONFIRM.classList.remove('error');
         ERRORMSG.hidden = true;
     } else {
-        console.log('no');
         PASSWORD.classList.add('error');
         CONFIRM.classList.add('error');
         ERRORMSG.hidden = false;
